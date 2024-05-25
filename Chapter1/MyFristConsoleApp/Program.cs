5a866bf44433469b5e53b8c465127e65b51bbd77
SUN <forten99@gmail.com>
2024-05-25 15:42:33 +0900

    Head First C# Initial Commit

---
+++ Chapter1/MyFristConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFristConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
